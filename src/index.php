e25e559389cb58016eaaa642acd9225f7a0634eb
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-02 13:36:10 +0200

    Base Scaffolding

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>php-oop-1</title>
+</head>
+<body>
+    <h1>hi!</h1>
+    <script src="./js/script.js"></script>
+</body>
+</html>
