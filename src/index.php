73a4fe266dffbc698c68625789087447e382d34c
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-05 18:38:45 +0200

    Stampo in pagina i dati creati dei film

--- src/index.php
+++ src/index.php
@@ -1,13 +1,14 @@
 <?php
 
-include __DIR__ . '/src/models/movie.php';
+require_once __DIR__ . '/models/movie.php';
 
-$mConfidenza = new Movie ( "Confidenza", "Drammatico", "Confidenza-locandina" , "7/10");
+$mConfidenza = new Movie ( "Confidenza", "Drammatico", "Confidenza-locandina.jpg" , "7/10");
 
 $mInsideOut2 = new Movie ( "INSIDE OUT 2", "Animazione", "/src/img/INSIDE_OUT 2-locandina.jpg" , "8/10");
 
 $mAnimaliRandagi = new Movie ( "ANIMALI RANDAGI", "Drammatico", "/src/img/Animali_Randagi-locandina.jpg" , "7/10");
 
+var_dump($mConfidenza, $mInsideOut2, $mAnimaliRandagi);
 ?>
 
 
@@ -21,7 +22,14 @@ $mAnimaliRandagi = new Movie ( "ANIMALI RANDAGI", "Drammatico", "/src/img/Animal
     <title>php-oop-1</title>
 </head>
 <body>
-    <h1>hi!</h1>
+    <h1>Lista film</h1>
+    <?php ?>
+        <ul>
+            <li>
+            <?php ?>
+            </li>
+        </ul>
+        <?php ?>
     <script src="./js/script.js"></script>
 </body>
 </html>
