8193d7dace7bd64e76462c1bcd3e05df70482295
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-09 10:56:43 +0200

    Stampare in pagina i dati dei film aggiunti alla classe usando il metodo echo

--- src/index.php
+++ src/index.php
@@ -2,13 +2,21 @@
 
 require_once __DIR__ . '/models/movie.php';
 
-$mConfidenza = new Movie ( "Confidenza", "Drammatico", "Confidenza-locandina.jpg" , "7/10");
+$moviesList = [
+    new Movie ( "Confidenza", "Drammatico", "http://localhost/php-oop-1/src/img/Confidenza-locandina.jpg" , "7/10"),
+    new Movie ( "INSIDE OUT 2", "Animazione", "http://localhost/php-oop-1/src/img/INSIDE_OUT 2-locandina.jpg" , "8/10"),
+    new Movie ( "ANIMALI RANDAGI", "Drammatico", "http://localhost/php-oop-1/src/img/Animali_Randagi-locandina.jpg" , "7/10"),
+];
 
-$mInsideOut2 = new Movie ( "INSIDE OUT 2", "Animazione", "/src/img/INSIDE_OUT 2-locandina.jpg" , "8/10");
+// $mConfidenza = new Movie ( "Confidenza", "Drammatico", "Confidenza-locandina.jpg" , "7/10");
 
-$mAnimaliRandagi = new Movie ( "ANIMALI RANDAGI", "Drammatico", "/src/img/Animali_Randagi-locandina.jpg" , "7/10");
+// $mInsideOut2 = new Movie ( "INSIDE OUT 2", "Animazione", "/src/img/INSIDE_OUT 2-locandina.jpg" , "8/10");
 
-var_dump($mConfidenza, $mInsideOut2, $mAnimaliRandagi);
+// $mAnimaliRandagi = new Movie ( "ANIMALI RANDAGI", "Drammatico", "/src/img/Animali_Randagi-locandina.jpg" , "7/10");
+
+// var_dump($mConfidenza, $mInsideOut2, $mAnimaliRandagi);
+
+// echo $mConfidenza->getTitle();
 ?>
 
 
@@ -23,13 +31,20 @@ var_dump($mConfidenza, $mInsideOut2, $mAnimaliRandagi);
 </head>
 <body>
     <h1>Lista film</h1>
-    <?php ?>
+    <section>
+    <?php foreach ($moviesList as $movieList) { ?>
         <ul>
             <li>
-            <?php ?>
+                <h2> <?php echo $movieList -> getTitle() ?> </h2>
+                <img src="<?php echo $movieList -> getImgPoster() ?>" alt="<?php echo $movieList -> getTitle() ?>">
+            </li>
+            <li>
+                <p>Genere: <?php echo $movieList -> getGenere() ?></p>
+                <p>Voto: <?php echo $movieList -> getVote() ?></p>
             </li>
         </ul>
-        <?php ?>
+    <?php } ?>
+    </section>
     <script src="./js/script.js"></script>
 </body>
 </html>
